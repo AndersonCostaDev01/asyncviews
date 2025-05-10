c1a50c878f24669f891e383cbbb7404ac63fc4ff
Anderson Costa <costaandersom23@gmail.com>
2025-05-10 18:47:11 -0300

    atividade feita

--- asyncviews/views.py
+++ asyncviews/views.py
@@ -1,6 +1,22 @@
 import asyncio
-import httpx
+from django.shortcuts import redirect
 from django.http import HttpResponse
+from asyncio import sleep
+import time
+
+import httpx
+
+async def home(request):
+    start = time.time()
+    await sleep(2)
+    end = time.time()
+    duration = end - start
+    
+    return redirect(f'timer/?duration={duration:.2f}')
+    
+def timer(request):
+    duration = request.GET.get('duration')
+    return HttpResponse(f'<h1>Tempo de execução: {duration}</h1><br><a href="/async_view">va para a pagina async</a>')
 
 async def http_call_async():
     for num in range(1, 6):
@@ -10,18 +26,18 @@ async def http_call_async():
         r = await client.get("https://httpbin.org/")
         print(r)
 
+async def async_view(request):
+    loop = asyncio.get_event_loop()
+    loop.create_task(http_call_async())
+    return HttpResponse(f"<h1>Async view</h1> <br> <a href='/sync_view'>veja o sincrono</a>")
+
 def http_call_sync():
     for num in range(1, 6):
-        asyncio.sleep(1)
+        sleep(1)
         print(num)
     r = httpx.get("https://httpbin.org/")
     print(r)
 
-async def async_view(request):
-    loop = asyncio.get_event_loop()
-    loop.create_task(http_call_async())
-    return HttpResponse("Non-blocking HTTP request")
-
 def sync_view(request):
     http_call_sync()
-    return HttpResponse("Blocking HTTP request")
+    return HttpResponse(f"<h1>Sync view</h1> <br> <a href='/async_view'>veja o assíncrono</a>")
